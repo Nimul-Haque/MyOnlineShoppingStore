fa99547f2397d2f8f735462331c47d13f52b7f5b
NimulHaque17301136 <103769025+NimulHaque17301136@users.noreply.github.com>
2022-04-28 02:24:53 +0600

    Add files via upload

---
+++ AdminControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineShoppingStore.Controllers;
+using System;
+
+namespace TestShop.UnitTests
+{
+    [TestClass]
+    public class AdminControllerTests
+    {
+        [TestMethod]
+        public void UpdateCategory_UserIsAdmin_ReturnsTrue()
+        {
+            var AdminController = new AdminController();
+           
+        }
+        public void CategoryEdit_UserIsAdmin_ReturnsTrue()
+        {
+            var AdminController = new AdminController();
+
+        }
+        public void Product_UserIsAdmin_ReturnsTrue()
+        {
+            var AdminController = new AdminController();
+
+        }
+
+    }
+}
